e36d22c88e887bf434c3dc562f727e99051ceec4
jacky0207 <jackylam0207@gmail.com>
2023-11-27 00:22:16 -0500

    fixed power of ten and root commands previous number throw exception for not exist

--- Sources/ScientificCalculator-iOS/ControlPanel/Command/Function/ScientificCalculatorInverseLogCommand.swift
+++ Sources/ScientificCalculator-iOS/ControlPanel/Command/Function/ScientificCalculatorInverseLogCommand.swift
@@ -8,12 +8,20 @@
 import Foundation
 
 class ScientificCalculatorPowerOfTenCommand: ScientificCalculatorCommand {
+    override func previousNumberType() -> CalculatorCommandType {
+        return .optional
+    }
+
     override func answer(left: Double, right: Double) throws -> Double {
         return left * pow(10, right)
     }
 }
 
 class ScientificCalculatorExponentialCommand: ScientificCalculatorCommand {
+    override func previousNumberType() -> CalculatorCommandType {
+        return .optional
+    }
+    
     override func answer(left: Double, right: Double) throws -> Double {
         return left * exp(right)
     }
--- Sources/ScientificCalculator-iOS/ControlPanel/Command/Function/ScientificCalculatorRootCommand.swift
+++ Sources/ScientificCalculator-iOS/ControlPanel/Command/Function/ScientificCalculatorRootCommand.swift
@@ -8,18 +8,30 @@
 import Foundation
 
 class ScientificCalculatorRootCommand: ScientificCalculatorCommand {
+    override func previousNumberType() -> CalculatorCommandType {
+        return .optional
+    }
+
     override func answer(left: Double, right: Double) throws -> Double {
         return pow(right, 1/left)
     }
 }
 
 class ScientificCalculatorSquareRootCommand: ScientificCalculatorCommand {
+    override func previousNumberType() -> CalculatorCommandType {
+        return .optional
+    }
+
     override func answer(left: Double, right: Double) throws -> Double {
         return left * sqrt(right)
     }
 }
 
 class ScientificCalculatorCubeRootCommand: ScientificCalculatorCommand {
+    override func previousNumberType() -> CalculatorCommandType {
+        return .optional
+    }
+    
     override func answer(left: Double, right: Double) throws -> Double {
         return left * cbrt(right)
     }
