da90310e2a35b598b5ffc75648c10c999d381a39
jacky0207 <jackylam0207@gmail.com>
2023-12-11 01:22:46 -0500

    added calculator angle text and toggle function

--- Sources/ScientificCalculator-iOS/ControlPanel/Param/CalculatorParam.swift
+++ Sources/ScientificCalculator-iOS/ControlPanel/Param/CalculatorParam.swift
@@ -9,7 +9,18 @@ public enum CalculatorParam {
     case angle
 }
 
-public enum CalculatorAngle {
-    case degree
-    case radian
+public enum CalculatorAngle: String {
+    case degree = "Deg"
+    case radian = "Rad"
+}
+
+extension CalculatorAngle {
+    public mutating func toggle() {
+        switch self {
+        case .degree:
+            self = .radian
+        case .radian:
+            self = .degree
+        }
+    }
 }
